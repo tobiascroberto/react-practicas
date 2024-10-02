3185491c85f796beb570d138a3d2848299813224
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-10-02 17:01:38 -0600

    modificacion hola mundo

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -19,15 +19,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Saludos "Holi"  {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        Hola Mundo!!!      </p>
     </>
   )
 }
