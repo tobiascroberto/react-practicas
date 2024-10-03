61639ba54af25fe3828deab67c5ab9d89246fbc1
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-10-03 10:02:28 -0600

    se agrego un titulo randomizado

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,15 @@
+const mensajes = ["Fundamentos React","React Principios", "React Bases"]
+
+
+function NumeroAleatorio (min,max){
+  return Math.floor(Math.random()*(max-min+1)+min)
+}
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-logo-xs.png" alt="React logo" />
-      <h1>React Fundamentos</h1>
+      <h1> {mensajes[NumeroAleatorio(0,2)]} </h1>
       <p>
         ¡Conceptos fundamentales de React que necesitas conocer para desarrollar
         cualquier app con esta famosa librería!
