23fb8245b8866f4e78e3ea63e79f4f003b1a65db
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-10-04 16:31:04 -0600

    version con props y componentes en carpetas

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,39 @@
-const mensajes = ["Fundamentos React","React Principios", "React Bases"]
-
-
-function NumeroAleatorio (min,max){
-  return Math.floor(Math.random()*(max-min+1)+min)
-}
-
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-logo-xs.png" alt="React logo" />
-      <h1> {mensajes[NumeroAleatorio(0,2)]} </h1>
-      <p>
-        ¡Conceptos fundamentales de React que necesitas conocer para desarrollar
-        cualquier app con esta famosa librería!
-      </p>
-    </header>
-  );
-}
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts.jsx';
+import { CORE_CONCEPTS } from './data.js'; 
+import TabButton from './components/TabButton/TabButton.jsx';
 
 function App() {
+  function handleClickMenu(){
+    console.log("Ey! Estas pulsando un boton de mi menu!");
+  }
   return (
+
     <div>
-      <Header />
+     
+      <Header/>
+      <section id="coreConcepts">
+        <h2>Principales Características</h2>
+        <div>
+        
+        <CoreConcepts {...CORE_CONCEPTS[0]}/>
+        <CoreConcepts {...CORE_CONCEPTS[1]}/>
+        <CoreConcepts {...CORE_CONCEPTS[2]}/>
+        <CoreConcepts imagePath={CORE_CONCEPTS[3].imagePath}  title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description}/>
+        
+       
+        </div>
+      </section>
+      <section id="reactExamples">
+        <h2>Ejemplos React</h2>
+        <menu>
+         <TabButton onClick={handleClickMenu}>Componentes</TabButton>
+         <TabButton onClick={handleClickMenu}>JSX</TabButton>
+         <TabButton onClick={handleClickMenu}>Props</TabButton>
+         <TabButton onClick={handleClickMenu}>Estados</TabButton>
+        </menu>
+        Contenido Dinamico
+      </section>
       <main>
         <h2>¡Comenzamos Picando Código en React!</h2>
       </main>
