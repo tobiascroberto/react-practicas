a4be7d944b853a84f8bf32c34a11bc64c7d85fd0
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-10-09 18:29:19 -0600

    nuevo proyecto en react vie

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,11 @@
-import Header from "./components/Header/Header.jsx";
-import ReactExamples from "./components/ReactExamples/ReactExamples.jsx";
-import CoreConceptsSelector from "./components/CoreConceptsSelector/CoreConceptsSelector.jsx";
+
 
 function App() {
  
 
   return (
     <>
-      <Header />
-      <CoreConceptsSelector />
-      <ReactExamples />
-      <main>
-        <h2>¡Comenzamos Picando Código en React!</h2>
-      </main>
+    <h1>Holas!!</h1>
     </>
   );
 }
