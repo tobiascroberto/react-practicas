98bc85dbb0e4bdd66388082da2b85a7c02822637
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-10-09 18:34:45 -0600

    actualizacion de archivos

--- p01_jsx-properties/TabButton/TabButton.jsx
+++
@@ -1,9 +0,0 @@
-import './TabButton.css';
-export default function TabButton({children, onClick,isSelected}) {
-
-  return (
-    <li>
-      <button className={isSelected?'active':''} onClick={onClick}>{children}</button>
-    </li>
-  );
-}
